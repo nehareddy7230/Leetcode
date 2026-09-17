dfb7e85e6dfd754178a5854f44827b1a24a25391
nehareddy7230 <pullagurlaneha1@gmail.com>
2026-09-17 20:08:05 +0530

    Time: 69 ms (26.91%), Space: 134.9 MB (29.6%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.java
@@ -1,33 +1,46 @@
+import java.util.*;
+
 class Solution {
     public int minSumOfLengths(int[] arr, int target) {
         int n = arr.length;
         int INF = 1000000;
+
         int[] best = new int[n];
         Arrays.fill(best, INF);
-        int left = 0;
-        int sum = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int prefix = 0;
         int ans = INF;
-        int minLen = INF;
 
-        for (int right = 0; right < n; right++) {
-            sum += arr[right];
+        for (int i = 0; i < n; i++) {
+            prefix += arr[i];
 
-            while (sum > target && left <= right) {
-                sum -= arr[left++];
-            }
+            int need = prefix - target;
 
-            if (sum == target) {
-                int len = right - left + 1;
+            if (map.containsKey(need)) {
+                int start = map.get(need);
+                int len = i - start;
 
-                if (left > 0 && best[left - 1] != INF) {
-                    ans = Math.min(ans, len + best[left - 1]);
+                if (start >= 0 && best[start] != INF) {
+                    ans = Math.min(ans, len + best[start]);
                 }
 
-                minLen = Math.min(minLen, len);
+                if (i == 0) {
+                    best[i] = len;
+                } else {
+                    best[i] = Math.min(best[i - 1], len);
+                }
+            } else {
+                if (i > 0) {
+                    best[i] = best[i - 1];
+                }
             }
 
-            best[right] = minLen;
+            map.put(prefix, i);
         }
+
         return ans == INF ? -1 : ans;
     }
 }
